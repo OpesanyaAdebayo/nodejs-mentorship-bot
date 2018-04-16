871bb30854b345b38f466efc12a56d8e1a324875
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-04-16 01:24:34 +0100

    removed useless files

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/events', events);
 
